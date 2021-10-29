1541e0a031e9c5c23380398ff9d64628eb37e22a
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2021-10-29 22:03:59 +0300

    Исправил недочет подсчета количества натуральных чисел

--- Natur_num/Program.cs
+++ Natur_num/Program.cs
@@ -11,32 +11,34 @@ uint k = 12;
 uint l = 2;
 
 int count = 0;
-
+uint num_nat = 0;
 while (count < n)
 {
     if (count % k == 0 && count % l == 0)
     {
-        Console.WriteLine(count);
+        num_nat++;
     }
     count ++;
-}*/
+}
+Console.WriteLine(num_nat);*/
 
 // Делятся хотя бы на одно значение
 
 /*uint n = 256;
 uint k = 13;
 uint l = 9;
-
+uint num_nat = 0;
 int count = 0;
 
 while (count < n)
 {
     if (count % k == 0 || count % l == 0)
     {
-        Console.WriteLine(count);
+       nat_num++;
     }
     count ++;
-}*/
+}
+ Console.WriteLine(num_nar);*/
 
 // не делятся на сумму k + l
 
@@ -45,12 +47,13 @@ uint k = 1;
 uint l = 1;
 
 int count = 0;
-
+uint nat_num = 0;
 while (count < n)
 {
     if (count % (k+l) != 0 )
     {
-        Console.WriteLine(count);
+        nat_num++;
     }
     count ++;
-}*/
+}
+ Console.WriteLine(nat_num);*/
